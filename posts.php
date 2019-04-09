1cd7d22c6c10048859f70c2cdbc42725882d205f
ibalnih <nuriqbald@gmail.com>
2019-04-09 10:23:18 +0700

    second

---
+++ posts.php
@@ -0,0 +1,37 @@
+<?php
+
+//Membuat koneksi ke database
+include_once 'db.php';
+
+$sql = "SELECT * FROM post";
+$result = $mysqli->query($sql);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Posts</title>
+</head>
+<body>
+    <h1>Posts</h1>
+
+    <?php
+
+    // Jika tabel kosong/tidak ada post
+    if (!$result->num_rows) {
+        echo 'No posts.';
+    // Jika ada post, tampilkan
+    } else {
+        while ($post = $result->fetch_object()) {
+            echo "<h3>$post->title</h3>";
+            echo "<p align=\"justify\">$post->content</p>";
+        }
+    }
+
+    ?>
+</body>
+</html>
